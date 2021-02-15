e3b29d02ec46741cd7a130e873c08403ca281f8d
herry88 <herry081288@gmail.com>
2021-02-15 08:02:50 +0700

    AddFileCekLogin

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>halaman index</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css" type="text/css">
 </head>
 <body>
     <h2>Halaman Login</h2>
-    <form action="" method="post">
+    <form action="cek_login.php" method="post">
         <table>
             <tr>
                 <td>Username</td>
