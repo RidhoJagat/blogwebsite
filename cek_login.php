1a04249d8367808af4f9daaab24e1a2116fe1a4a
herry88 <herry081288@gmail.com>
2021-02-15 08:51:00 +0700

    AddNewCode

--- cek_login.php
+++ cek_login.php
@@ -6,4 +6,13 @@ include "config/koneksi.php";
 $username = $_POST['username'];
 $password = $_POST['password'];
 
+//cek query database
+$cek = "SELECT * FROM users WHERE username = '$username' 
+        AND password = '$password' AND blokir = 'N'";
+$login = mysqli_query($conn, $cek);
+$ketemu = mysqli_num_rows($login);
+$data = mysqli_fetch_array($login);
+//Apabila username dan password ditemukan (valid)
+// if(){}
+
 ?>
