44e7850e0bc4a425743fce12f9db73e75e9a37e8
herry88 <herry081288@gmail.com>
2021-02-15 11:16:49 +0700

    CekLoginDone

--- cek_login.php
+++ cek_login.php
@@ -13,6 +13,17 @@ $login = mysqli_query($conn, $cek);
 $ketemu = mysqli_num_rows($login);
 $data = mysqli_fetch_array($login);
 //Apabila username dan password ditemukan (valid)
-// if(){}
+if($ketemu > 0){
+        session_start(); //untuk memulai session
+        //bikin variabel session 
+        $_SESSION['namauser'] = $data['username'];
+        $_SESSION['passuser'] = $data['password'];
+        //mengarahkan ke halaman dashboar
+        // header("location: dashboar.php");
+        echo "<h1>Anda berhasil login</h1>";
+} else {
+        echo "<center>Username dan password tidak diketahui</center><br>";
+        echo "<center><a href='index.php'>Silahkan login kembali</a></center>";
+}
 
 ?>
